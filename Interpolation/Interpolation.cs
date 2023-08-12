180c95ee0f4aa0a843dbdb3b81b935cdfef82acd
Emre Guzel <91648268+Jonfoble@users.noreply.github.com>
2023-08-12 16:00:15 +0300

    Refactoring Interpolation.cs
    Better commentation and refactoring.

--- Interpolation/Interpolation.cs
+++ Interpolation/Interpolation.cs
@@ -2,28 +2,36 @@ using UnityEngine;
 
 public static class Interpolation
 {
+    // Constants to clamp dot product
+    private const float MinDotProductValue = -1.0f;
+    private const float MaxDotProductValue = 1.0f;
+
+    /// <summary>
+    /// Interpolates between two vectors either linearly or spherically based on the specified parameter.
+    /// </summary>
+    /// <param name="a">Start vector.</param>
+    /// <param name="b">End vector.</param>
+    /// <param name="t">Interpolation parameter.</param>
+    /// <param name="linear">If set to <c>true</c>, use linear interpolation. Otherwise, use spherical interpolation.</param>
+    /// <returns>Interpolated vector.</returns>
     public static Vector3 Interpolate(Vector3 a, Vector3 b, float t, bool linear = false)
     {
-        if (linear)
-        {
-            return Vector3.Lerp(a, b, Mathf.Clamp01(t));
-        }
-        else
-        {
-            // Dot product - the cosine of the angle between 2 vectors.
-            float dot = Vector3.Dot(a, b);
+        return linear ? LinearInterpolate(a, b, t) : SphericalInterpolate(a, b, t);
+    }
 
-            // Clamp to be in the range of Acos() because its a floating value
-            dot = Mathf.Clamp(dot, -1.0f, 1.0f);
+    private static Vector3 LinearInterpolate(Vector3 a, Vector3 b, float t)
+    {
+        return Vector3.Lerp(a, b, Mathf.Clamp01(t));
+    }
 
-            // Acos(dot) returns the angle between a and b and multiplying that by t returns the angle between start and the final result.
-            float theta = Mathf.Acos(dot) * t;
+    private static Vector3 SphericalInterpolate(Vector3 a, Vector3 b, float t)
+    {
+        float dot = Vector3.Dot(a, b);
+        dot = Mathf.Clamp(dot, MinDotProductValue, MaxDotProductValue);
 
-            // Calculate the relative vector and normalize it
-            Vector3 relativeVec = (b - a * dot).normalized;
+        float theta = Mathf.Acos(dot) * t;
+        Vector3 relativeVec = (b - a * dot).normalized;
 
-            // The final result using spherical linear interpolation (slerp)
-            return (a * Mathf.Cos(theta)) + (relativeVec * Mathf.Sin(theta));
-        }
+        return (a * Mathf.Cos(theta)) + (relativeVec * Mathf.Sin(theta));
     }
 }
